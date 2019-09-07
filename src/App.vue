1443f8ff52069d1369edc7cfe44596756c0f3f3c
Pedro Gomes <pedrompg311@gmail.com>
2019-09-07 17:36:21 +0100

    Base

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,29 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <FullPageSection :section-title="'Telhados'"></FullPageSection>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import FullPageSection from './components/FullPageSection.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    FullPageSection
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import './styles/styles.scss';
+
+  #app {
+     font-family: 'Avenir', Helvetica, Arial, sans-serif;
+     -webkit-font-smoothing: antialiased;
+     -moz-osx-font-smoothing: grayscale;
+     text-align: center;
+     color: #2c3e50;
+     margin-top: 60px;
+   }
 </style>
---
+++ src/components/FullPageSection.vue
@@ -0,0 +1,35 @@
+<template>
+  <section class="hero is-link is-fullheight-with-navbar">
+    <div class="hero-body">
+      <div class="container">
+        <p class="title">
+          {{sectionTitle}}
+        </p>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'FullPageSection',
+  props: {
+    sectionTitle: {
+      type: String,
+      required: true
+    }
+  },
+  data () {
+    return {
+    }
+  },
+  computed: {
+  },
+  methods: {
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
